0967359ba9e58e4d01d55d4126bf27b17126b02a
ShivajeetR <rai.shivajeet@gmail.com>
2022-08-19 13:57:09 +0100

    added home screen with message and profile

--- navigators/RootStack.tsx
+++ navigators/RootStack.tsx
@@ -2,12 +2,19 @@ import React, {FunctionComponent} from 'react';
 
 //screens
 import Welcome from '../screens/Welcome';
+import Home from '../screens/Home';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import {colors} from '../components/colors';
+import Greeting from '../components/Header/Greeting';
+import Profile from '../components/Header/Profile';
+import avi from '../assets/avatar.png';
+
 export type RootStackParamList = {
   Welcome: undefined;
+  Home: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -15,11 +22,49 @@ const Stack = createStackNavigator<RootStackParamList>();
 const RootStack: FunctionComponent = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.graylight,
+            borderBottomWidth: 0,
+            shadowColor: 'transparent',
+            shadowOpacity: 0,
+            elevation: 0,
+            height: 120,
+          },
+          headerTintColor: colors.secondary,
+          headerRightContainerStyle: {
+            paddingRight: 25,
+          },
+          headerLeftContainerStyle: {
+            paddingLeft: 10,
+          },
+          headerRight: () => (
+            <Profile
+              img={avi}
+              imgContainerStyle={{backgroundColor: colors.tertiary}}
+            />
+          ),
+        }}
+        initialRouteName="Home">
         <Stack.Screen
           name="Welcome"
           component={Welcome}
           options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerTitle: props => (
+              <Greeting
+                mainText="Hey John Doe!"
+                subText="Welcome Back!"
+                {...props}
+              />
+            ),
+            headerLeft: () => <></>,
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
